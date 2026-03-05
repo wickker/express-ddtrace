71727b96208082de70401a3ae8463e5f2e7e6d72
Choy Dian Chun <choydianchun@gmail.com>
2026-03-05 16:34:14 +0800

    first commit

---
+++ src/server.ts
@@ -0,0 +1,42 @@
+import "./tracer"; // MUST be first import
+import express from "express";
+import { logger } from "./logger.js";
+
+const app = express();
+app.use(express.json());
+
+app.use((req, res, next) => {
+	const { method, originalUrl } = req;
+	const { statusCode } = res;
+
+	res.on("finish", () => {
+		logger.info("Http request", {
+			method,
+			path: originalUrl,
+			status: statusCode,
+		});
+	});
+
+	next();
+});
+
+app.get("/", (_req, res) => {
+	res.send("DIAN-EXPRESS-APP IS UP AND RUNNING");
+});
+
+app.get("/health", (_req, res) => {
+	res.json({ message: "health ok" });
+});
+
+app.get("/users", (_req, res) => {
+	res.json([
+		{ id: 1, username: "alice", email: "alice@example.com" },
+		{ id: 2, username: "bob", email: "bob@example.com" },
+		{ id: 3, username: "charlie", email: "charlie@example.com" },
+	]);
+});
+
+const port = 9000;
+app.listen(port, () => {
+	logger.info(`Listening on port: `, { port });
+});
