1be7a1d5352b879d20167f0c25845ea5c4f77eb5
Choy Dian Chun <choydianchun@gmail.com>
2026-03-05 17:18:22 +0800

    add dk port

--- dist/tracer.js
+++ dist/tracer.js
@@ -1,6 +1,7 @@
 import tracer from "dd-trace";
 tracer.init({
     logInjection: true,
+    port: 9529
 });
 export default tracer;
 //# sourceMappingURL=tracer.js.map
--- src/tracer.ts
+++ src/tracer.ts
@@ -2,6 +2,7 @@ import tracer from "dd-trace";
 
 tracer.init({
 	logInjection: true,
+    port: 9529
 });
 
 export default tracer;
